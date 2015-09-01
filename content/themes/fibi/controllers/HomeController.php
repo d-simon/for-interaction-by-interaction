f609bdf4e351ef85e242b159650dbf8b8dcea4c5
David Simon <me@davidsimon.ch>
2015-08-31 23:19:34 -0300

    [Fix] HomeController: Get future posts (next one first) otherwise fallback to last event

--- content/themes/fibi/controllers/HomeController.php
+++ content/themes/fibi/controllers/HomeController.php
@@ -11,11 +11,22 @@ final class HomeController extends AbstractController
 	public function getContext()
 	{
 
+		// Get published events (minus $post_current)
 		$post_current = $this->getPostForArgs([
 			'post_type' 		=> 'event',
-			'post_status'		=> ['publish', 'future']
+			'post_status'		=> ['future'],
+			'orderby'			=> 'date',
+			'oder'				=> 'ASC'
 		]);
 
+		// Otherwise get newest post
+		if (!$post_current) {
+			$post_current = $this->getPostForArgs([
+				'post_type' 		=> 'event'
+			]);
+		}
+
+		// Get published events (minus $post_current)
 		$posts_archive = $this->getPostsForArgs([
 			'post_type' 		=> 'event',
 			'posts_per_page' 	=> 3,
