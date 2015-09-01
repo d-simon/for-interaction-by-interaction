5134b245ce9ebf72576dad021b567ac930f83515
David Simon <me@davidsimon.ch>
2015-09-01 11:29:32 -0300

    [Chore] Whitespace: Spaces -> Tabs

--- theme/gulp/html/default.js
+++ theme/gulp/html/default.js
@@ -8,17 +8,17 @@
 var gulp = require('gulp');
 
 var taskName = 'html',
-    taskConfig = {
-        src: './source/{,demo/}{,pages/**/,modules/**/,preview/styleguide/**/}!(_)*.twig',
-        includes: './source/{,demo/}{,layouts/,modules/**/,preview/**/}*.twig',
-        dest: './build/',
-        watch: [
-            'source/{,demo/}{,layouts/,pages/**/,modules/**/,preview/**/}*.twig',
-            'source/{,demo/}{,layouts/,pages/**/,modules/**/,preview/**/}*.data.js',
-            'source/{,demo/}modules/**/*.md',
-            'source/assets/css/data/colors.json'
-        ]
-    };
+	taskConfig = {
+		src: './source/{,demo/}{,pages/**/,modules/**/,preview/styleguide/**/}!(_)*.twig',
+		includes: './source/{,demo/}{,layouts/,modules/**/,preview/**/}*.twig',
+		dest: './build/',
+		watch: [
+			'source/{,demo/}{,layouts/,pages/**/,modules/**/,preview/**/}*.twig',
+			'source/{,demo/}{,layouts/,pages/**/,modules/**/,preview/**/}*.data.js',
+			'source/{,demo/}modules/**/*.md',
+			'source/assets/css/data/colors.json'
+		]
+	};
 
 gulp.task(taskName, function(cb) {
 	var helpers = require('require-dir')('../../helpers'),
@@ -30,22 +30,22 @@ gulp.task(taskName, function(cb) {
 		tap = require('gulp-tap'),
 		rename = require('gulp-rename'),
 		// prettify = require('gulp-prettify'),
-        twig = require('gulp-twig');
+		twig = require('gulp-twig');
 
-    var fileExists = function(path) {
-            // From https://www.npmjs.com/package/is-there
-            try {
-                fs.statSync(path);
+	var fileExists = function(path) {
+			// From https://www.npmjs.com/package/is-there
+			try {
+				fs.statSync(path);
 
-                return true;
-            } catch (err) {
-                return (err.code !== 'ENOENT');
-            };
-        },
+				return true;
+			} catch (err) {
+				return (err.code !== 'ENOENT');
+			};
+		},
 		twigFunctions = [
 			{
 				/**
-				 * This function is simply a mockup of proxy function for
+				 * This function is simply a mockup of a proxy function for
 				 * get_field which enables us to use the same template in
 				 * the frontend and backend by implementing the same
 				 * function in Twig.js and Timber.
@@ -61,44 +61,44 @@ gulp.task(taskName, function(cb) {
 			base: './source'
 		})
 		.pipe(tap(function(file) {
-            var dataFile = util.replaceExtension(file.path, '.data.js'),
-                data = (function() {
-                    if (fileExists(dataFile)) {
-                        try {
-                            return requireNew(dataFile);
-                        } catch (err) {
-                            helpers.errors({
-                                task: taskName,
-                                message: 'Error reading "' + path.relative('./', dataFile) + '": ' + err
-                            });
-                        }
-                    } else if (util.env.dev) {
-                        console.log('[Info] No data file "' + path.relative('./', dataFile) + '" found.');
-                    }
-                })(),
-                modulePrepend = new Buffer('{% extends "preview/layouts/module.twig" %}{% block content %}'),
-                moduleAppend = new Buffer('{% endblock %}');
+			var dataFile = util.replaceExtension(file.path, '.data.js'),
+				data = (function() {
+					if (fileExists(dataFile)) {
+						try {
+							return requireNew(dataFile);
+						} catch (err) {
+							helpers.errors({
+								task: taskName,
+								message: 'Error reading "' + path.relative('./', dataFile) + '": ' + err
+							});
+						}
+					} else if (util.env.dev) {
+						console.log('[Info] No data file "' + path.relative('./', dataFile) + '" found.');
+					}
+				})(),
+				modulePrepend = new Buffer('{% extends "preview/layouts/module.twig" %}{% block content %}'),
+				moduleAppend = new Buffer('{% endblock %}');
 
-            // Save data by file name
-            file.data = data;
+			// Save data by file name
+			file.data = data;
 
-            // Wrap modules with custom layout for preview purposes
-            if (file.path.indexOf(path.sep + 'modules' + path.sep) !== -1) {
-                file.contents = Buffer.concat([modulePrepend, file.contents, moduleAppend]);
-            }
-        }))
+			// Wrap modules with custom layout for preview purposes
+			if (file.path.indexOf(path.sep + 'modules' + path.sep) !== -1) {
+				file.contents = Buffer.concat([modulePrepend, file.contents, moduleAppend]);
+			}
+		}))
 		.pipe(plumber())
-        .pipe(twig({
-            data: function(file) {
-                return file.data;
-            },
-            base: './source/',
-            includes: taskConfig.includes,
-            functions: twigFunctions,
-            getIncludeId: function(filePath) {
-                return path.relative('./source', filePath);
-            }
-        }).on('error', helpers.errors))
+		.pipe(twig({
+			data: function(file) {
+				return file.data;
+			},
+			base: './source/',
+			includes: taskConfig.includes,
+			functions: twigFunctions,
+			getIncludeId: function(filePath) {
+				return path.relative('./source', filePath);
+			}
+		}).on('error', helpers.errors))
 		// Relativify absolute paths
 		.pipe(tap(function(file) {
 			var content = file.contents.toString(),
