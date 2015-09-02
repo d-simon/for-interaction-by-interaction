922852351becf26b28e20f1b76ebf26a03a8dfb6
David Simon <me@davidsimon.ch>
2015-09-01 22:11:41 -0300

    [Update] Event: Add sample image

--- theme/source/modules/event/event.data.js
+++ theme/source/modules/event/event.data.js
@@ -16,6 +16,7 @@ var data = _.merge(defaultData, {
 			post_date: '2015-05-06 18:00:26',
 			event_excerpt: 'iart gestaltet medientechnische Installationen und berücksichtigt dabei sowohl visuelle als auch inhaltliche und narrative Aspekte, um Menschen und Räume durch Interaktion miteinander zu verbinden. ',
 			content: 'Seit 2001 realisiert iart Projekte im Spannungsfeld von Medien, Kunst und Technologie mit dem Schwerpunkt Interaktion im Raum. Mit dem Hintergrund eines Ingenieurbüros stellt iart sich komplexen Gestaltungsaufgaben als interdisziplinäres Kompetenzzentrum aus den Bereichen Management, Design, Engineering und Servicing. Die im Team vereinten Kompetenzen und die langjährige Projekterfahrung lassen ermöglichen es dem Team, flexibel auf die Bedürfnisse der vielseitigen Projekte aus Kultur, Architektur und Industrie einzugehen.',
+			image: '/tmp/media/event-archive-1.jpg',
 			event_guests: [{
 				'event_guest_name': 'iart',
 				'event_guest_type': 'organisation',
