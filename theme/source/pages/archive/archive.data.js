16e023f1cd704be1a6028ce44f4c8cee48f8ed16
David Simon <me@davidsimon.ch>
2015-09-01 21:29:37 -0300

    [Update] Pages: Scaffold all page types

---
+++ theme/source/pages/archive/archive.data.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var _ = require('lodash'),
+	requireNew = require('require-new'),
+	defaultData = requireNew('../../data/default.data.js'),
+	eventData = requireNew('../../modules/event/event.data.js');
+
+var data = _.merge(defaultData, {
+		meta: {
+			title: 'Archive'
+		},
+		title: 'Archive',
+		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+		posts: [
+			eventData.event,
+			eventData.event,
+			eventData.event
+		]
+	});
+
+module.exports = data;
