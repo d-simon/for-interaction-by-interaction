f718a6e70721a0f1e7c988d9ed24057a0a6ba6f2
David Simon <me@davidsimon.ch>
2015-09-02 20:05:21 -0300

    [Update] Page / Section: add page modifier to enable standard page heading styling of section__title

--- theme/source/pages/contact/contact.data.js
+++ theme/source/pages/contact/contact.data.js
@@ -8,7 +8,7 @@ var data = _.merge(defaultData, {
 		meta: {
 			title: 'Contact'
 		},
-		title: 'Contact',
+		title: 'Kontakt',
 		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
 	});
 
