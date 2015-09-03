d9a2178d4dbb4a8faf8c81ace86a5414fc81a919
David Simon <me@davidsimon.ch>
2015-09-02 21:40:21 -0300

    [Update] Logo: link to home

--- theme/source/data/default.data.js
+++ theme/source/data/default.data.js
@@ -9,7 +9,8 @@ var util = require('gulp-util'),
 			project: {
 				title: 'For Interaction By Interaction',
 				lead: 'For Interaction By Interaction – kurz <b>FIBI</b> – ist eine Event-Reihe der Studierenden der Vertiefung Interaction Design der ZHdK. Wir bieten eine Plattform des Austauschs, auf der Studierende, Alumnis und Industrie aufeinander treffen.'
-			}
+			},
+			home_url: '/'
 		},
 		mainnav: require('../modules/mainnav/mainnav.data.js').mainnav
 	};
