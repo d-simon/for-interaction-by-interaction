0c1b673b0540c99669a604d5f263b477bbf34fb2
David Simon <me@davidsimon.ch>
2015-09-06 13:36:21 -0400

    [Feature] Eventdetail

--- theme/source/modules/eventdetail/eventdetail.data.js
+++ theme/source/modules/eventdetail/eventdetail.data.js
@@ -3,14 +3,19 @@
 var _ = require('lodash'),
 	requireNew = require('require-new'),
 	dataHelper = require('../../../helpers/data.js'),
-	defaultData = requireNew('../../data/default.data.js');
+	defaultData = requireNew('../../data/default.data.js'),
+	eventData = requireNew('../event/event.data.js');
 
 var data = _.merge(defaultData, {
 		meta: {
 			title: 'Event-Detail',
 			code: dataHelper.getTemplateCode('eventdetail.twig'),
 			documentation: dataHelper.getDocumentation('eventdetail.md')
-		}
+		},
+		event: _.merge(eventData.event, {
+			event_location: 'ZHdK, Toni Areal, 5.K03',
+			content: 'Seit 2001 realisiert iart Projekte im Spannungsfeld von Medien, Kunst und Technologie mit dem Schwerpunkt Interaktion im Raum. Mit dem Hintergrund eines Ingenieurbüros stellt iart sich komplexen Gestaltungsaufgaben als interdisziplinäres Kompetenzzentrum aus den Bereichen Management, Design, Engineering und Servicing. Die im Team vereinten Kompetenzen und die langjährige Projekterfahrung lassen ermöglichen es dem Team, flexibel auf die Bedürfnisse der vielseitigen Projekte aus Kultur, Architektur und Industrie einzugehen.'
+		})
 	});
 
 module.exports = data;
--- theme/source/pages/event/event.data.js
+++ theme/source/pages/event/event.data.js
@@ -2,14 +2,15 @@
 
 var _ = require('lodash'),
 	requireNew = require('require-new'),
-	defaultData = requireNew('../../data/default.data.js');
+	defaultData = requireNew('../../data/default.data.js'),
+	eventdetailData = requireNew('../../modules/eventdetail/eventdetail.data.js');;
 
 var data = _.merge(defaultData, {
 		meta: {
 			title: 'Event'
 		},
 		title: 'Event',
-		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+		post: eventdetailData.event
 	});
 
 module.exports = data;
