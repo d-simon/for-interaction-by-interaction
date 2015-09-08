f93a0ac59808098721d02c94b8467c4e2b896e80
David Simon <me@davidsimon.ch>
2015-09-08 14:52:42 -0400

    [Update] Styleguide/Home: Make "Home" bold and move it to first place in the stylguide

--- theme/source/pages/home/home.data.js
+++ theme/source/pages/home/home.data.js
@@ -8,7 +8,7 @@ var _ = require('lodash'),
 
 var data = _.merge(defaultData, {
 		meta: {
-			title: 'Home'
+			title: '<b>Home</b>'
 		},
 		title: 'Home',
 		text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
