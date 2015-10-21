76ff28588dbe956147dda19f74fd75cd165f1d43
David Simon <me@davidsimon.ch>
2015-10-21 18:43:52 +0200

    [Update] Eventdetail: Differentiate between "current" and "archive"

--- content/themes/fibi/controllers/EventSingleController.php
+++ content/themes/fibi/controllers/EventSingleController.php
@@ -11,8 +11,22 @@ final class EventSingleController extends AbstractController
 {
 	public function getContext()
 	{
+
+		$post = $this->getPost();
+
+		$post_newest = $this->getPostForArgs([
+			'post_type' => 'event',
+			'post_status' => ['publish','future']
+		]);
+
+		$is_current = false;
+		if ($post->status == 'future' || $post_newest->ID == $post->ID) {
+			$is_current = true;
+		}
+
         return [
         	'title' => get_the_title(),
+        	'is_current' => $is_current,
         	'post' => $this->getPost()
     	];
 	}
